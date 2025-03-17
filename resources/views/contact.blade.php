ffa3599b90c8fe2880b4ace0711501048d704c24
Yaroslav <121134813+yarikprp@users.noreply.github.com>
2025-03-17 20:55:08 +0300

    Добавил две страницы контакты и о нас

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2 class="text-3xl font-bold">Contact Us</h2>
+    <p class="mt-4 text-lg text-gray-600">Email: support@website.com</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
-
-Route::view('/second', 'second');
+Route::view('contact', 'contact');
+Route::view('about', 'about');
