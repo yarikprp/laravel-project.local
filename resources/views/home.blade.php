7c0702da0ba98fceabb7c351df947ba9d0102fb5
Yaroslav <121134813+yarikprp@users.noreply.github.com>
2025-03-17 20:49:16 +0300

    Сделал домашнюю страницу

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-100 text-gray-900">
 <header class="bg-white shadow-md py-4">
