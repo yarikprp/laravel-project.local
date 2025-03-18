54b6cff4deeb374a053aa17470eba63d149fd17f
Yaroslav <121134813+yarikprp@users.noreply.github.com>
2025-03-18 10:20:16 +0300

    Сделал управление категориями

--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,3 @@ Route::get('posts/{post}', [PostController::class, 'show'])
     ->name('post.show');
 Route::view('contact', 'contact')->name('contact');
 Route::view('about', 'about')->name('about');
-
