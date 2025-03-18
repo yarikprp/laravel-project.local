70a4ed818192c80659b221df5aacf6c28a682ac9
Yaroslav <121134813+yarikprp@users.noreply.github.com>
2025-03-18 10:26:49 +0300

    Сгруппировал маршруты

--- project2/routes/web.php
+++ project2/routes/web.php
@@ -8,16 +8,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-Route::resource('categories', CategoryController::class);
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->middleware(['verified'])->name('dashboard');
+
+    Route::resource('categories', CategoryController::class);
+});
 
 require __DIR__.'/auth.php';
